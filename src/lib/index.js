c4850d6349dc792bf6a7309c403687da586b9bbe
TNThung <hung11112002@gmail.com>
2023-07-05 14:53:09 +0800

    [Fix] not type not exported correctly

--- src/lib/index.js
+++
@@ -1,2 +0,0 @@
-// Reexport your entry components here
-export { writable } from "./writable.js";
---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+// Reexport your entry components here
+export { writable } from "./writable.js";
+
+export type {
+  Subscriber,
+  WritableConfig,
+  BetterReadable,
+  BetterWritable,
+  StartStopNotifier,
+} from "./writable.d.ts";
