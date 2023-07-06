b0a790c0a41b3aa217e2548087e9c669decbe62d
TNThung <hung11112002@gmail.com>
2023-07-06 15:30:19 +0800

    [Chg] format file better

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,13 @@
 <script context="module" lang="ts">
   import { writable } from "$lib/writable.js";
-  const count = writable(0, {
-    "trackerCount": 3,
-  });
-
-  const {
-    previous: [prev1],
-  } = count;
 
 
-  count.subscribe((n, [o1, o2, o3]) => {
-    console.log(`count: ${o3} -> ${o2} -> ${o1} -> ${n}`);
-  });
+  const count = writable(0, { "trackerCount": 2 });
+  const { previous: [prev1] } = count;
+
+
+  count.subscribe((n, [o1, o2]) =>
+    console.log(`${o2} -> ${o1} -> ${n}`));
 </script>
 
 
